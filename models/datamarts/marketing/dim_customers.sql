4b6bf406781a6d444cc2ceb925376ceefb05d76b
Natraj Puligilla <npuligil@hotmail.com>
2024-12-26 17:48:56 +0000

    ...

--- models/datamarts/marketing/dim_customers.sql
+++ models/datamarts/marketing/dim_customers.sql
@@ -1,19 +1,11 @@
 with 
-
 customers as (
-
      select * from {{ ref('stg_jaffle_shop__customers') }}
-
 ),
-
 orders as ( 
-
     select * from {{ ref('stg_jaffle_shop__orders') }}
-
 ),
-
 customer_orders as (
-
     select
         customer_id,
 
@@ -26,9 +18,7 @@ customer_orders as (
     group by 1
 
 ),
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -37,11 +27,8 @@ final as (
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) 
         as number_of_orders
-
     from customers
-
     left join customer_orders using (customer_id)
-
 )
 
 select * from final
